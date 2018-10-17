8353f5cb48045247c949dd02f53f8b1393abfa16
Anna Półtorak <aniajtp@gmail.com>
2018-10-17 22:26:49 +0200

    add modules

---
+++ modules/OSinfo.js
@@ -0,0 +1,22 @@
+var os = require('os');
+var colors = require('colors');
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log(colors.gray('System:'), type);
+    console.log(colors.red('Release:'), release);
+    console.log(colors.magenta('CPU model:'), cpu);
+    console.log(colors.blue('Uptime: ~'), (uptime / 60).toFixed(0), 'min');
+    console.log(colors.yellow('User name:'), userInfo.username);
+    console.log(colors.green('Home dir:'), userInfo.homedir);
+}
+exports.print = getOSinfo;
